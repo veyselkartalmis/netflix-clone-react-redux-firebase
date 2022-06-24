859d5b083f242861e15082cf5070e0a4d8cb9699
Veysel <veyselkartalms@gmail.com>
2022-06-25 01:05:52 +0300

    firabase config file added

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCLQnjCGSZxMB5iyQssCuCtlSHkZ_ZkSR4",
+    authDomain: "netflix-clone-6ae96.firebaseapp.com",
+    projectId: "netflix-clone-6ae96",
+    storageBucket: "netflix-clone-6ae96.appspot.com",
+    messagingSenderId: "271069388357",
+    appId: "1:271069388357:web:4f5a8975adfa8c80090d2c"
+};
