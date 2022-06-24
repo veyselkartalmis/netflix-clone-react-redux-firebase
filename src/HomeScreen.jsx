5013af9d69da648340430b829b21f91a0f44b5b3
Veysel <veyselkartalms@gmail.com>
2022-06-25 02:15:22 +0300

    navbar & navbar transition addded

--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -1,11 +1,12 @@
+import Navbar from "./components/Navbar/Navbar";
 import "./homeScreen.scss";
 
 function HomeScreen() {
-  return (
-      <div className="homeScreen"> 
-          HomeScreen
-      </div>
-  )
+    return (
+        <div className="homeScreen">
+            <Navbar />
+        </div>
+    )
 }
 
-export default HomeScreen
+export default HomeScreen;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from "react";
 import "./navbar.scss";
 
 function Navbar() {
-  return (
-      <div className="nav">
-          Navbar
-      </div>
-  )
+    const [show, handleShow] = useState(false);
+
+    const transitionNavbar = () => {
+        if (window.scrollY > 100) {
+            handleShow(true);
+        } else {
+            handleShow(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavbar);
+        return () => window.removeEventListener("scroll", transitionNavbar);
+    }, []);
+
+    return (
+        <div className={`nav ${show && "nav__black"}`}>
+            <div className="nav__contents">
+                <img className="nav__logo" src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt="netflix-logo" />
+                <img className="nav__avatar" src="https://avatars.githubusercontent.com/u/6759280?v=4" alt="" />
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
