c9cd6b3e6f7c779e16ad192361240cc3d702ccab
Veysel <veyselkartalms@gmail.com>
2022-06-26 18:19:39 +0300

    api added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,6 +1,8 @@
 import "./banner.scss";
+import { useState } from "react";
 
 function Banner() {
+
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n - 1) + "..." : string;
     }
