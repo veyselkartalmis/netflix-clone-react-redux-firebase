bba849e790129dbe7d6d9200d8b3b9fb434bee9a
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-06-27 18:53:46 +0300

    register & signIn function added

--- src/screens/SignInScreen/SignUpScreen.jsx
+++ src/screens/SignInScreen/SignUpScreen.jsx
@@ -1,16 +1,24 @@
 import "./signUpScreen.scss";
 
 function SignUpScreen() {
+	const register = (e) => {
+		e.preventDefault();
+	}
+
+	const signIn = (e) => {
+		e.preventDefault();
+	}
+
 	return (
 		<div className="signUpScreen">
 			<form>
 				<h1>Sign In</h1>
 				<input type="email" placeholder="Email" />
 				<input type="password" placeholder="Password" />
-				<button type="submit">Sign In</button>
+				<button type="submit" onClick={signIn}>Sign In</button>
 				<h4>
 					<span className="signUpScreen__gray">New to Netflix? </span>
-					<span className="signUpScreen__link">Sign Up Now.</span> 
+					<span className="signUpScreen__link" onClick={register}>Sign Up Now.</span> 
 				</h4>
 			</form>
 		</div>
