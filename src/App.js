b48f7c43ab1d877f2808b91303caa344be1d36cb
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-06-28 18:59:25 +0300

    useEffect added for login

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 import HomeScreen from "./screens/HomeScreen/HomeScreen";
 import LoginScreen from "./screens/LoginScreen/LoginScreen";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { auth } from "./firebase";
 
 function App() {
-  const user = null;
+	const user = null;
 
-  return (
-    <div className="app">
-      {!user ? (
-        <LoginScreen />
-      ) : (
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<HomeScreen />}></Route>
-          </Routes>
-        </BrowserRouter>
-      )}
-    </div>
-  );
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+			if (userAuth) {
+				//Logged In
+			} else {
+				//Logged Out
+			}
+		});
+
+		return unsubscribe;
+	}, []);
+
+	return (
+		<div className="app">
+			{!user ? (
+				<LoginScreen />
+			) : (
+				<BrowserRouter>
+					<Routes>
+						<Route exact path="/" element={<HomeScreen />}></Route>
+					</Routes>
+				</BrowserRouter>
+			)}
+		</div>
+	);
 }
 
 export default App;
