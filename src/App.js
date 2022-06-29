4a8ef0549b3551eb6060ad498fc92672e846ec98
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-06-29 18:58:02 +0300

    logIn & logOut dispatch added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import HomeScreen from "./screens/HomeScreen/HomeScreen";
 import LoginScreen from "./screens/LoginScreen/LoginScreen";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./features/userSlice";
 
 function App() {
 	const user = null;
+	const dispatch = useDispatch();
 
 	useEffect(() => {
 		const unsubscribe = auth.onAuthStateChanged((userAuth) => {
 			if (userAuth) {
 				//Logged In
+				dispatch(login({
+					uid: userAuth.uid,
+					email: userAuth.email,
+				}));
 			} else {
 				//Logged Out
+				dispatch(logout);
 			}
 		});
 
