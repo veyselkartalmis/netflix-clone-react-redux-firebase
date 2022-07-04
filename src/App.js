ba9333e1a9da65abd075adb3b1ac6bf0c295c86a
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-07-04 18:49:03 +0300

    editProfile added

--- src/App.js
+++ src/App.js
@@ -2,29 +2,31 @@ import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen/HomeScreen";
 import LoginScreen from "./screens/LoginScreen/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen/ProfileScreen";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { auth } from "./firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-
 	const user = useSelector(selectUser);
 	const dispatch = useDispatch();
 
 	useEffect(() => {
 		const unsubscribe = auth.onAuthStateChanged((userAuth) => {
 			if (userAuth) {
-				dispatch(login({
-					uid: userAuth.uid,
-					email: userAuth.email,
-				}));
+				dispatch(
+					login({
+						uid: userAuth.uid,
+						email: userAuth.email,
+					})
+				);
 			} else {
-				dispatch(logout);
+				dispatch(logout());
 			}
 		});
 		return unsubscribe;
-	}, []);
+	}, [dispatch]);
 
 	return (
 		<div className="app">
@@ -34,6 +36,7 @@ function App() {
 				<BrowserRouter>
 					<Routes>
 						<Route exact path="/" element={<HomeScreen />}></Route>
+						<Route path="/profile" element={<ProfileScreen />}></Route>
 					</Routes>
 				</BrowserRouter>
 			)}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import { React, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./navbar.scss";
 
 function Navbar() {
 	const [show, handleShow] = useState(false);
+	const history = useNavigate();
 
 	const transitionNavbar = () => {
 		if (window.scrollY > 100) {
@@ -21,11 +23,13 @@ function Navbar() {
 		<div className={`nav ${show && "nav__black"}`}>
 			<div className="nav__contents">
 				<img
+				onClick={() => history("/")}
 					className="nav__logo"
 					src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
 					alt="netflix-logo"
 				/>
 				<img
+					onClick={() => history("/profile")}
 					className="nav__avatar"
 					src="https://avatars.githubusercontent.com/u/6759280?v=4"
 					alt=""
